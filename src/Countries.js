a405f381ae8e21406741a09b955768ce60920102
ellie <ellie.tms1371@gmail.com>
2020-05-30 11:08:58 +0100

    update card-body and card-text-container

--- src/Countries.js
+++ src/Countries.js
@@ -7,14 +7,14 @@ const Countries = (props) => {
       <div className="row">
           {props.AllCountries.map((eachcountry,index) => {
             return (
-            <div className="col-7  mx-auto col-sm-9  col-md-6 col-lg-3 col-xl-3 mb-sm-2 mb-md-2 mt-md-3 mb-lg-3 mt-lg-3 p-2">    
+            <div className="col-7  mx-auto col-sm-9  col-md-6 col-lg-3 col-xl-3 mb-sm-2 mb-md-2 mt-md-3 mb-lg-3 mt-lg-3 p-2 page">    
             <div key={index} className="card mt-4 mx-auto">
             <div className="card-img">    
             <img className="card-img-top" src={eachcountry.flag} alt="" />
             </div>  
             <div class="card-body">
-            <h5 class="card-title">{eachcountry.name}</h5>
-                <div class="d-flex flex-column">
+            <h5 class=" card-text-container">{eachcountry.name}</h5>
+                <div class="card-text-container">
                     <p className="card-text">Population :{eachcountry.population.toString().replace(/(.)(?=(\d{3})+$)/g,'$1,')}</p>
                     <p className="card-text">Region :{eachcountry.region}</p>
                     <p className="card-text">Capital :{eachcountry.capital}</p>
