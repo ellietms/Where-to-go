7ea3c5e24248e60352bc9a2daca8dd3da3d0f0eb
ellie <ellie.tms1371@gmail.com>
2020-05-30 13:13:54 +0100

    add changes for search

--- src/Search.js
+++ src/Search.js
@@ -5,11 +5,14 @@ import 'bootstrap/dist/css/bootstrap.css';
 const Search = () => {
 return( 
     <div className="mt-5 col-7 col-sm-9  col-md-6 col-lg-3 col-xl-3 mb-sm-2 mb-md-2 mb-lg-3 mt-lg-3 p-2 ">
-    <form className="form-inline">
-    <div className="input-group">
-    <input type="text" className="form-control" placeholder="Search"/>
-    </div>
-    </form>
+        <nav class="navbar d-flex col-12 col-sm-12">
+            <form >
+                    <div class="inputWithIcon">
+                    <i class="fas fa-search-location"></i>
+                    <input type="text" id="searchInput" class="form-control" placeholder="search for a country..." aria-label="Username" aria-describedby="basic-addon1"/>
+                    </div>
+            </form>
+            </nav>
     </div>    
 )
 }
