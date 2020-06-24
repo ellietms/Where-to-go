62e3b64631e2b9037e74ebc69c3043cffb61d561
ellie <ellie.tms1371@gmail.com>
2020-06-24 19:23:39 +0100

    Update MainPage.js

--- src/MainPage.js
+++ src/MainPage.js
@@ -9,63 +9,60 @@ import Search from "./Search";
 const MainPAge = () => {
   const [searchcountry, setSearchCountry] = useState("");
   const [continentFilter, setContinentFilter] = useState(null);
-  const [countryName,setCountryName] = useState(null);
+  const [countryName, setCountryName] = useState(null);
 
-
-
-  function setMyCountryName(country){
+  function setMyCountryName(country) {
     setCountryName(country);
   }
 
-  //This function is not complete and does not make buttons   
-  function getNameOfCountryBorders(){
-       return (Data.filter(country => countryName.borders.includes(country.alphaCode.toUpperCase()))
-       .map(country => (   
-            <button type="button" className="mr-1 btn btn-sm btn-outline-dark"> 
-            <p class="content pt-1">{country.name}</p>
-            </button>
-            )))
-  }   
-         
-  
+  //This function is not complete and does not make buttons
+  function getNameOfCountryBorders() {
+    return Data.filter((country) =>
+      countryName.borders.includes(country.alphaCode.toUpperCase())
+    ).map((country) => (
+      <button type="button" className="mr-1 btn btn-sm btn-outline-dark">
+        <p class="content pt-1">{country.name}</p>
+      </button>
+    ));
+  }
+
   const filteredCountries = Data.filter((country) =>
     continentFilter === null ? true : country.region === continentFilter
-    ).filter((country) =>
+  ).filter((country) =>
     searchcountry === ""
       ? true
       : country.name.toLowerCase().includes(searchcountry) ||
         country.capital.toLowerCase().includes(searchcountry)
-    )
-   let mainContent;
-    if(countryName === null){
-     mainContent = ( 
-        <div>     
-        <Header/>
-        <Search
-        value={searchcountry}
-        handleChange={(event) => {setSearchCountry(event.target.value)}}
-        handleRegion={(region) => setContinentFilter(region)}
-        handleAll={() => setContinentFilter(null)}
-        />
-    
-        <Countries countries={filteredCountries} 
-        setMyCountryName = {(eachcountry) => setMyCountryName(eachcountry)}
-        />
-    
-        </div>
-        )
-    }
-    else{
-      mainContent = ( 
-      < CountryInfo country={countryName}
-      getNameOfCountryBorders={() => getNameOfCountryBorders()}
-      handlePage = {() =>  setCountryName(null)}/>
-      )
-    }
-return (
-    <div> 
-    {mainContent}
-    </div>
   );
-}
+  let mainContent;
+  if (countryName === null) {
+    mainContent = (
+      <div>
+        <Header />
+        <Search
+          value={searchcountry}
+          handleChange={(event) => {
+            setSearchCountry(event.target.value);
+          }}
+          handleRegion={(region) => setContinentFilter(region)}
+          handleAll={() => setContinentFilter(null)}
+        />
+
+        <Countries
+          countries={filteredCountries}
+          setMyCountryName={(eachcountry) => setMyCountryName(eachcountry)}
+        />
+      </div>
+    );
+  } else {
+    mainContent = (
+      <CountryInfo
+        country={countryName}
+        getNameOfCountryBorders={() => getNameOfCountryBorders()}
+        handlePage={() => setCountryName(null)}
+      />
+    );
+  }
+  return <div>{mainContent}</div>;
+};
 export default MainPAge;
