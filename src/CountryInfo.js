a2c934b61a831e46d79c6a321b61d8b5ba8e7ad1
ellie <ellie.tms1371@gmail.com>
2021-01-21 00:22:26 +0000

    add changes for css

--- src/CountryInfo.js
+++ src/CountryInfo.js
@@ -14,20 +14,22 @@ const CountryInfo = ({
         <div className="mx-5 mb-5">
           <button
             type="button"
-            className="border border-secondary"
+            className="back-button"
             onClick={() => showAllCountries()}
           >
             Back
           </button>
         </div>
         <div className="image-container mt-5">
-          <img className="countryImg" src={country.flag}  alt="country"/>
+          <img className="countryImg" src={country.flag} alt="country" />
         </div>
-        <div className="information-country">
-          <h5 className="mt-xs-4 pt-sm-4 mt-sm-4 mt-md-3 mt-lg-3 m-auto">
-            {country.name}
-          </h5>
-          <div className="mt-sm-5 d-flex justify-content-between">
+        <div>
+          <div className="country-name">
+            <h5>
+              {country.name}
+            </h5>
+          </div>
+          <div className="d-flex">
             <div className="ml-sm-0 mr-sm-4 mr-md-1 col-sm-4 country-details">
               <p>
                 <strong>Native Name:</strong>
@@ -50,7 +52,7 @@ const CountryInfo = ({
                 {country.capital}
               </p>
             </div>
-            <div class=" d-flex flex-column  col-sm-6 country-details">
+            <div class=" d-flex flex-column  col-sm-6 country-details ml-5">
               <p class="d-flex">
                 <strong id="details-right" class="mr-1">
                   Top Level Domain:
@@ -58,33 +60,33 @@ const CountryInfo = ({
                 {country.topLevelDomain[0]}
               </p>
               <p class=" d-flex">
-                <strong id="details-right">Currencies:</strong>
+                <strong id="details-right"> Currencies:</strong>
                 {country.currencies.map((currency) => currency.code)}
               </p>
               <div class="d-flex">
-                <strong id="details-right">Languages:</strong>
+                <strong id="details-right"> Languages:</strong>
                 <p class="flex-sm-column">
                   {country.languages.map((language) => language.name)}
                 </p>
               </div>
-            </div>
-            <div class="col-12">
-              <p
-                class="d-flex d-lg-inline-flex mr-lg-0 col-lg-4
-                     mt-sm-2 mr-sm-2 mb-sm-0"
-              >
-                <strong class="borders">Border Countries:</strong>{" "}
-              </p>
-              <span
-                class="col-lg-6 mt-sm-1 mt-md-0 d-sm-flex 
+              <div class="d-flex flex-column">
+                <p class="d-flex">
+                  <strong>Border Countries:</strong>
+                </p>
+                <div
+                  class="mt-sm-1 mt-md-0 d-sm-flex 
                     flex-md-column flex-lg-row d-lg-inline-flex"
-              >
-                {borderCountries.map((country) => (
-                  <button onClick={() => showBorderCountry(country)}>
-                    {country.name}
-                  </button>
-                ))}
-              </span>
+                >
+                  {borderCountries.map((country) => (
+                    <button
+                      className="border-buttons"
+                      onClick={() => showBorderCountry(country)}
+                    >
+                      {country.name}
+                    </button>
+                  ))}
+                </div>
+              </div>
             </div>
           </div>
         </div>
